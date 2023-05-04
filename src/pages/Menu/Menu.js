c30b8b9d6a1866667857cf160fef267329b48d79
MahdiHasan943 <mahdifreelancer41@gmail.com>
2023-05-04 23:32:08 +0600

    added Menu bg and wine bg

--- src/pages/Menu/Menu.js
+++ src/pages/Menu/Menu.js
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import style from '../../styles/Menu.module.css'
 const Menu = () => {
   return (
-    <div>menu</div>
+    <>
+      <div className={`${style.MmenuBg} dark:opacity-[0.5] h-[70vh]`}>
+        <div className=" flex justify-center h-full items-center">
+          <h1 className='text-white text-center text-[45px]  sm:text-[68px] sm:leading-[84px]'>MENU</h1>
+          </div>
+
+      </div>
+      
+    </>
   )
 }
 
--- src/pages/WineAndCoctail/WineAndCoctail.js
+++ src/pages/WineAndCoctail/WineAndCoctail.js
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import style from '../../styles/WineAndCoctail.module.css'
 const WineAndCoctail = () => {
   return (
-    <div>WineAndCoctail</div>
+    <>
+    <div className={`${style.MwineBg} dark:opacity-[0.5] h-[70vh]`}>
+      <div className=" flex justify-center h-full items-center">
+        <h1 className='text-white text-center text-[45px]  sm:text-[68px] sm:leading-[84px]'>Wine & Cocktail Bar</h1>
+        </div>
+
+    </div>
+    
+  </>
   )
 }
 
