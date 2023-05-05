119ad42c9a0ffafbca8226b37700188666bc1e56
MahdiHasan943 <mahdifreelancer41@gmail.com>
2023-05-05 12:53:09 +0600

    loading

--- src/pages/Menu/[menuId].js
+++ src/pages/Menu/[menuId].js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useState } from 'react'
 
 const menuDetails = () => {
     const router = useRouter();
@@ -64,35 +64,37 @@ const menuDetails = () => {
     const filtered = menuCards.filter(card => {
         return card.id === menuId;
       });
-
+    const [loading, setLoading] = useState();
     
     return (
-      
-      <div className='heroBg h-[100vh] flex justify-center items-center'>
-            <h1 className='text-[red] text-5xl'>Details about product {menuId}</h1>
+        <div className="">
+              {loading?<h1>..lading</h1>:<div className='heroBg h-[100vh] flex justify-center items-center'>
+      <h1 className='text-[red] text-5xl'>Details about product {menuId}</h1>
 
 
-            <div className="">
-                {
-                   filtered.map(card=><div className='px-10 ' key={card.id} >
+      <div className="">
+          {
+             filtered.map(card=><div className='px-10 ' key={card.id} >
 
-                   <div className="px-8 py-8 sm:py-20 sm:px-20 border border-[#B5986D]">
-                   <img className='w-full h-[80%]' src={card.image} alt="" />
-               
-                    </div>
-                   <p className='text-[#B5986D] text-center pt-10 pb-4 font-bold text-[32px]'>{card.name}</p>
-                            
-               
-                 </div>) 
-                }
-
-                </div>
-  
-  
+             <div className="px-8 py-8 sm:py-20 sm:px-20 border border-[#B5986D]">
+             <img className='w-full h-[80%]' src={card.image} alt="" />
          
-        
-          
+              </div>
+             <p className='text-[#B5986D] text-center pt-10 pb-4 font-bold text-[32px]'>{card.name}</p>
+                      
+         
+           </div>) 
+          }
+
+          </div>
+
+
+   
+  
+    
+</div>}
     </div>
+      
   )
 }
 
