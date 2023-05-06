aaefecce8aa41b72481160f4eea5e1090ee4a0d8
MahdiHasan943 <mahdifreelancer41@gmail.com>
2023-05-06 13:13:05 +0600

    added wine details page's information

--- src/pages/WineAndCoctail/Softdrinks.js
+++ src/pages/WineAndCoctail/Softdrinks.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import Reservation from '@/Components/Reservation'
 
 const Softdrinks = () => {
   return (
-    <div>Softdrinks</div>
+    <div className=''>
+
+                 <div className='h-[550px] bg-no-repeat  dark:opacity-[0.8] bg-cover bg-full bg-center w-full' style={{ backgroundImage: `url("../wineList.png")` }} >
+                 <div className=" flex justify-center h-full items-center">
+          <h1 className='text-white text-center  text-[45px]  sm:text-[90px] sm:leading-[100px]'>Wine & Cocktail List</h1>
+          </div>
+                  </div>
+
+               
+
+                 <Reservation></Reservation>
+                 
+                      
+         
+           </div>
   )
 }
 
