ce1c32d3497e0d93f8799d82042854dd3beffc02
MahdiHasan943 <mahdifreelancer41@gmail.com>
2023-06-04 21:31:02 +0600

    added event button

--- src/Components/imageSlide/ImageSlide.js
+++ src/Components/imageSlide/ImageSlide.js
@@ -11,6 +11,8 @@ import "swiper/css/navigation";
 import { EffectCreative, Navigation, Pagination } from "swiper";
 const elementSize = 500;
 
+
+
 import { PhotoProvider, PhotoView } from "react-photo-view";
 
 const ImageSlide = () => {
@@ -125,7 +127,7 @@ const ImageSlide = () => {
                         <div  {...attrs}>
                           <div className="  h-[100vh]" style={{ transform: `scale(${childScale})`, width: elementSize, transformOrigin: '0 0' }}>
                             <img className="w-full" src="./OurEventImage1.png" alt="" />
-                            <div className="mt-20 sm:mt-[50px]">
+                            <div className="mt-20 flex justify-center sm:mt-[50px]">
                             <a  className="dark:text-white text-black text-[15px] sm:text-[20px]   sm:my-0 dark:hover:bg-[#B5986D]  hover:bg-[black] hover:text-white  border-[#B5986D]  dark:border-white border-[1.6px] px-12 sm:px-20 py-7 rounded-2xl font-bold bookNow  outline-none focus:outline-none ">Book Event</a>
 
                             </div>
